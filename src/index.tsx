b42e4b3a8cf7dc87697aaacdb7b5cc0c134eb5ea
Hadi Albinsaad <hey@hadi.gg>
2022-08-01 14:10:30 +0300

    done profile page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { LoginPage } from './routes/login';
+import { ProfilePage } from './routes/profile';
 import './styles/index.css';
 import './styles/login.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<LoginPage></LoginPage>
+		<ProfilePage></ProfilePage>
 	</React.StrictMode>
 );
